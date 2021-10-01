299398cabfc11bfee864dfc0afc7b2454c58f86c
Allcharmian <91695211+Allcharmian@users.noreply.github.com>
2021-10-01 16:50:34 -0500

    Update example.spec.ts

--- postcomm/assembly/__tests__/example.spec.ts
+++ postcomm/assembly/__tests__/example.spec.ts
@@ -1,16 +1,9 @@
-import { hello_word } from "..";
-
-describe("example", () => {
-  it("should return 'hello word'", () => {
-    expect (hello_word()).toStrictEqual ("hello word");
-  })
-});
 
 import { makeCampaña, getCampañas, vote, makeExtendedCampaña } from '../../assembly';
 import { storage, Context, runtime_api, VMContext, logging } from "near-sdk-as";
 
 const someContract = "somecontract.testnet"
-
+const wcom = 'wcom.testnet';
 const allcharmian = "allcharmian.testnet"
 const bravio = "bravio.testnet"
 const lazio = "lazio.testnet"
@@ -30,19 +23,19 @@ describe("Campaña", () => {
     });
 
     it("should create Campaña", () => {
-        var Campaña = makeCampaña("test Campaña");
-        expect(Campaña.id).toStrictEqual(0)
+        var campaña = makeCampaña("test Campaña");
+        expect(campaña.id).toStrictEqual(0)
         expect(getCampañas('me').length).toStrictEqual(1);    
         expect(getCampañas('').length).toStrictEqual(0);
-        expect(getCampañas('me')[0].Campaña.who).toStrictEqual(allcharmian);
-        log("Campaña created by: " + getCampañas('me')[0].Campaña.who);    
+        expect(getCampañas('me')[0].campaña.who).toStrictEqual(allcharmian);
+        log("Campaña created by: " + getCampañas('me')[0].campaña.who);    
     });
 
     it("should create two Campañas by the same creator", () => {
-        var Campaña1 = makeCampaña("test Campaña");
-        expect(Campaña1.id).toStrictEqual(0)
-        var Campaña2 = makeCampaña("test Campaña 2");
-        expect(Campaña2.id).toStrictEqual(1)
+        var campaña1 = makeCampaña("test Campaña");
+        expect(campaña1.id).toStrictEqual(0)
+        var campaña2 = makeCampaña("test Campaña 2");
+        expect(campaña2.id).toStrictEqual(1)
         expect(getCampañas('me').length).toStrictEqual(2);    
         expect(getCampañas('').length).toStrictEqual(0);
         log("total Campañas created: " + "by " + allcharmian + ": " + getCampañas('me').length.toString());
@@ -53,16 +46,16 @@ describe("Campaña", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeCampaña("allcharmian's Campaña");
-        expect(Campaña1.Campaña.who).toStrictEqual(allcharmian);
+        var campaña1 = makeCampaña("allcharmian's Campaña");
+        expect(campaña1.campaña.who).toStrictEqual(allcharmian);
         expect(getCampañas('me').length).toStrictEqual(1);    
         expect(getCampañas('').length).toStrictEqual(0);    
 
         VMContext.setSigner_account_id(bravio)
         VMContext.setPredecessor_account_id(bravio)
 
-        var Campaña2 = makeCampaña("bravio's Campaña");
-        expect(Campaña2.Campaña.who).toStrictEqual(bravio);
+        var campaña2 = makeCampaña("bravio's Campaña");
+        expect(campaña2.campaña.who).toStrictEqual(bravio);
         expect(getCampañas('me').length).toStrictEqual(1);    
         expect(getCampañas('').length).toStrictEqual(1);    
 
@@ -77,140 +70,140 @@ describe("Campaña", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeCampaña("allcharmian's Campaña");
-        vote(Campaña1.id, true);
+        var campaña1 = makeCampaña("allcharmian's Campaña");
+        vote(campaña1.id, true);
     });
 
     it("should allow to vote for other's public Campaña", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeCampaña("allcharmian's Campaña");
+        var campaña1 = makeCampaña("allcharmian's Campaña");
 
         VMContext.setSigner_account_id(bravio)
         VMContext.setPredecessor_account_id(bravio)
 
-        Campaña1 = vote(Campaña1.id, true);
-        expect(Campaña1.Campaña.vote_yes).toStrictEqual(1);
-        expect(Campaña1.Campaña.vote_no).toStrictEqual(0);
+        campaña1 = vote(campaña1.id, true);
+        expect(campaña1.campaña.vote_yes).toStrictEqual(1);
+        expect(campaña1.campaña.vote_no).toStrictEqual(0);
 
-        Campaña1 = vote(Campaña1.id, true);
-        expect(Campaña1.Campaña.vote_yes).toStrictEqual(1);
-        expect(Campaña1.Campaña.vote_no).toStrictEqual(0);
+        campaña1 = vote(campaña1.id, true);
+        expect(campaña1.campaña.vote_yes).toStrictEqual(1);
+        expect(campaña1.campaña.vote_no).toStrictEqual(0);
 
-        Campaña1 = vote(Campaña1.id, false);
-        expect(Campaña1.Campaña.vote_yes).toStrictEqual(0);
-        expect(Campaña1.Campaña.vote_no).toStrictEqual(1);
+        campaña1 = vote(campaña1.id, false);
+        expect(campaña1.campaña.vote_yes).toStrictEqual(0);
+        expect(campaña1.campaña.vote_no).toStrictEqual(1);
 
         VMContext.setSigner_account_id(lazio)
         VMContext.setPredecessor_account_id(lazio)
 
-        Campaña1 = vote(Campaña1.id, false);
-        expect(Campaña1.Campaña.vote_yes).toStrictEqual(0);
-        expect(Campaña1.Campaña.vote_no).toStrictEqual(2);
+        campaña1 = vote(campaña1.id, false);
+        expect(campaña1.campaña.vote_yes).toStrictEqual(0);
+        expect(campaña1.campaña.vote_no).toStrictEqual(2);
 
-        Campaña1 = vote(Campaña1.id, false);
-        expect(Campaña1.Campaña.vote_yes).toStrictEqual(0);
-        expect(Campaña1.Campaña.vote_no).toStrictEqual(2);
+        campaña1 = vote(campaña1.id, false);
+        expect(campaña1.campaña.vote_yes).toStrictEqual(0);
+        expect(campaña1.campaña.vote_no).toStrictEqual(2);
     });
 
     itThrows("should disallow Campaña creation with invalid viewers", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeExtendedCampaña("allcharmian's Campaña", ["blablabla"], []);
-        vote(Campaña1.id, true);
+        var campaña1 = makeExtendedCampaña("allcharmian's Campaña", ["blablabla"], []);
+        vote(campaña1.id, true);
     });
 
     itThrows("should disallow Campaña creation with invalid voters", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeExtendedCampaña("allcharmian's Campaña", [], ["blablabla"]);
-        vote(Campaña1.id, true);
+        var campaña1 = makeExtendedCampaña("allcharmian's Campaña", [], ["blablabla"]);
+        vote(campaña1.id, true);
     });
 
     itThrows("should create private Campaña with no voters", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio], []);
-        vote(Campaña1.id, true);
+        var campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio], []);
+        vote(campaña1.id, true);
     });
 
     itThrows("should disallow to vote for own private Campaña if not in voters list", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio], []);
-        vote(Campaña1.id, true);
+        var campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio], []);
+        vote(campaña1.id, true);
     });
 
     it("should allow to vote for own private Campaña if in voters list", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio, allcharmian], [allcharmian]);
-        vote(Campaña1.id, true);
+        var campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio, allcharmian], [allcharmian]);
+        vote(campaña1.id, true);
     });
 
     itThrows("should disallow to vote for other's private Campaña if not in voters list", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio, allcharmian], [bravio]);
+        var campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio, allcharmian], [bravio]);
 
         VMContext.setSigner_account_id(lazio)
         VMContext.setPredecessor_account_id(lazio)
 
-        vote(Campaña1.id, true);
+        vote(campaña1.id, true);
     });
 
     it("should allow to vote for other's private Campaña if in voters list", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio, allcharmian], [bravio]);
+        var campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio, allcharmian], [bravio]);
 
         VMContext.setSigner_account_id(bravio)
         VMContext.setPredecessor_account_id(bravio)
 
-        vote(Campaña1.id, true);
+        vote(campaña1.id, true);
     });
 
     it("should return only private Campañas where in viewers list", () => {
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var Campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio], [bravio, allcharmian, smartio]);
-        expect(Campaña1.Campaña.canView.has(bravio)).toStrictEqual(true);
-        expect(Campaña1.Campaña.canView.has(allcharmian)).toStrictEqual(true);
-        expect(Campaña1.Campaña.canVote.has(bravio)).toStrictEqual(true);
-        expect(Campaña1.Campaña.canVote.has(allcharmian)).toStrictEqual(true);
+        var campaña1 = makeExtendedCampaña("allcharmian's Campaña", [bravio], [bravio, allcharmian, smartio]);
+        expect(campaña1.campaña.canView.has(bravio)).toStrictEqual(true);
+        expect(campaña1.campaña.canView.has(allcharmian)).toStrictEqual(true);
+        expect(campaña1.campaña.canVote.has(bravio)).toStrictEqual(true);
+        expect(campaña1.campaña.canVote.has(allcharmian)).toStrictEqual(true);
 
         VMContext.setSigner_account_id(lazio)
         VMContext.setPredecessor_account_id(lazio)
 
-        var laziosCampañas = getCampañas("others");
-        expect(laziosCampañas.length).toStrictEqual(0)
+        var lazioscampañas = getCampañas("others");
+        expect(lazioscampañas.length).toStrictEqual(0)
 
         VMContext.setSigner_account_id(bravio)
         VMContext.setPredecessor_account_id(bravio)
 
-        var braviosCampañas = getCampañas("others");
-        expect(braviosCampañas.length).toStrictEqual(1)
+        var bravioscampañas = getCampañas("others");
+        expect(bravioscampañas.length).toStrictEqual(1)
 
         VMContext.setSigner_account_id(allcharmian)
         VMContext.setPredecessor_account_id(allcharmian)
 
-        var allcharmiansCampañas = getCampañas("others");
-        expect(allcharmiansCampañas.length).toStrictEqual(1)
+        var allcharmianscampañas = getCampañas("others");
+        expect(allcharmianscampañas.length).toStrictEqual(1)
 
         VMContext.setSigner_account_id(smartio)
         VMContext.setPredecessor_account_id(smartio)
 
-        var smartiosCampañas = getCampañas("others");
-        expect(smartiosCampañas.length).toStrictEqual(1)
+        var smartioscampañas = getCampañas("others");
+        expect(smartioscampañas.length).toStrictEqual(1)
     });
 
 });
